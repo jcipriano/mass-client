f3270fe919b492090ce15ec94f3d227b960a96fd
jcipriano <cipriano.jonathan@gmail.com>
2013-08-03 01:08:25 -0700

    Adding files.

---
+++ source/javascripts/attend.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  var mass = {};
+  
+  mass.init = function() {
+    
+    this.pubnub = PUBNUB.init({
+        publish_key: 'pub-c-a7fcdbd9-359e-4df7-b71a-6e227656f161',
+        subscribe_key: 'sub-c-4ac38ffa-fc09-11e2-b598-02ee2ddab7fe'
+    });
+    
+    var that = this;
+    
+    this.pubnub.subscribe({
+        channel: "mass_channel",
+        connect: function(channel) {
+          that.onConnect(channel);
+        },
+        message: function(msg){
+          that.onMessage(msg);
+        }
+    })
+  };
+  
+  mass.onConnect = function(channel) {
+    console.log('Pubnub: Connected.')
+    this.pubnub.publish({
+      channel: "mass_channel",
+      message: {
+        type: 'ATTENDING'
+      }
+    });
+  };
+  
+  mass.onMessage = function(msg) {
+    console.log(msg);
+  };
+  
+  mass.init();
+
+});
